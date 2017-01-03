335590a07d44f7e8dac320f11088196e7c486d78
ouroumov <recon@s6n.org>
2017-01-03 09:40:18 +0100

    Initial commit

---
+++ runtests.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+
+import sys, doctest, string, StringIO
+from colors import colorString
+from tp_b1 import *
+
+def prompt_release_stdout(fakestdout):
+    # Function that prompts the user to realease content of fakestdout
+    while True:
+        action = raw_input("Print failed tests? [F(irst)/a(ll)/n(one)] ")
+        action = action.lower()
+        if action == '' or action == 'f':
+            print fakestdout.getvalue().split("*" * 70)[1]
+            break
+        elif action == 'a':
+            print fakestdout.getvalue()
+            break
+        elif action.lower() == 'n':
+            break
+
+
+
+
+
+
+fakestdout = StringIO.StringIO() # Fake file object for Stdout interception
+stdout = sys.stdout # Backup stdout
+
+
+targets = [
+    ("add_ints", add_ints),
+    ("sum_evens", sum_evens),
+    ("surround", surround),
+    ("center_in_field", center_in_field),
+    ("comment_block", comment_block),
+    ("is_nucleotide", is_nucleotide),
+    ("is_valid_dna_sequence", is_valid_dna_sequence),
+    ("get_complement", get_complement),
+    ("get_complement_sequence", get_complement_sequence),
+    ("get_number_from_letter", get_number_from_letter),
+    ("get_letter_from_number", get_letter_from_number)
+]
+
+print "=" * 79
+__test__ = {}
+for fname, f in targets:
+    __test__[fname] = f
+
+    # Intercept Stdout
+    sys.stdout = fakestdout
+    res = doctest.testmod()
+    # Restore Stdout
+    sys.stdout = stdout
+    test = string.ljust("{}".format(fname), 20, ' ') + " {}"
+
+    if res.failed == 0:
+        print colorString("green", test.format(res))
+    else:
+        print "=" * 79
+        print colorString("red", test.format(res))
+        print
+        prompt_release_stdout(fakestdout)
+        break
+
+    del __test__[fname]
+
+    if fname == "updateGrid":
+        print "-> You can now test main_v1.py"
+    elif fname == "doCmd":
+        print "-> You can now write main_v2.py"
+    elif fname == "updateGrids":
+        print "-> You can now write main_v3.py"
+
+print "=" * 79
+
---
+++ tp_b1.py
@@ -0,0 +1,202 @@
+#! /usr/bin/env python
+
+def add_ints(a, b):
+    ''' (int, int) -> int
+
+    Return the sum of a and b.
+
+    >>> add_ints(1, 1)
+    2
+    >>> add_ints(1, 3)
+    4
+    >>> add_ints(1, -6)
+    -5
+    '''
+    
+
+
+
+def sum_evens(n):
+    ''' (int) -> int
+
+    Return the sum of every even number from 0 to n (inclusive).
+
+    >>> sum_evens(4)
+    6
+    >>> sum_evens(5)
+    6
+    >>> sum_evens(6)
+    12
+    '''
+    
+
+
+
+def surround(c, s):
+    ''' (str, str) -> str
+
+    Returns s surrounded on both sides by c.
+
+    >>> surround('+', 'test')
+    '+test+'
+    >>> surround('!', 'B')
+    '!B!'
+    '''
+    
+
+
+
+
+def center_in_field(c, s, w):
+    ''' (str, str, int) -> str
+
+    Returns s centered in a field of characters c with length w.
+    If there's an odd number of characters in the field, add the extra character
+    on the right.
+
+    >>> center_in_field('.', 'lol', 5)
+    '.lol.'
+    >>> center_in_field('.', 'lol', 6)
+    '.lol..'
+    >>> center_in_field('+', 'lol', 7)
+    '++lol++'
+    >>> center_in_field(' ', 'lol', 8)
+    '  lol   '
+    '''
+    
+
+
+
+def comment_block(c, s, w):
+    ''' (str, str, int) -> str
+
+    >>> comment_block('#', 'test', 8)
+    '########\\n# test #\\n########'
+    >>> print comment_block('#', 'test', 10)
+    ##########
+    #  test  #
+    ##########
+    '''
+    
+
+
+def is_nucleotide(c):
+    ''' (str) -> Boolean
+
+    Return True if c in one of 'ATCG'
+
+    >>> is_nucleotide('A')
+    True
+    >>> is_nucleotide('B')
+    False
+    >>> is_nucleotide('1')
+    False
+    >>> is_nucleotide('!')
+    False
+    >>> is_nucleotide('G')
+    True
+    >>> is_nucleotide('C')
+    True
+    >>> is_nucleotide('T')
+    True
+    '''
+    
+
+
+
+def is_valid_dna_sequence(s):
+    ''' (str) -> Boolean
+    Return True if s is a valid dna sequence (contains only nucleotides)
+
+    >>> is_valid_dna_sequence('AT')
+    True
+    >>> is_valid_dna_sequence('ATF')
+    False
+    >>> is_valid_dna_sequence('TTATCCCG')
+    True
+    >>> is_valid_dna_sequence('TT1TCCCG')
+    False
+    '''
+    
+
+
+
+def get_complement(c):
+    ''' (str) -> str
+
+    Return the complement of nucleotide nuc.
+
+    >>> get_complement('A')
+    'T'
+    >>> get_complement('T')
+    'A'
+    >>> get_complement('C')
+    'G'
+    >>> get_complement('G')
+    'C'
+    '''
+    
+
+
+
+
+def get_complement_sequence(s):
+    ''' (str) -> str
+
+    Return the complementary sequence of DNA sequence s.
+
+    >>> get_complement_sequence('AT')
+    'TA'
+    >>> get_complement_sequence('TA')
+    'AT'
+    >>> get_complement_sequence('CCGCTAT')
+    'GGCGATA'
+    '''
+    
+
+
+
+
+def get_number_from_letter(c):
+    ''' (str) -> int
+    Return the number corresponding to letter c
+
+    >>> get_number_from_letter('A')
+    0
+    >>> get_number_from_letter('B')
+    1
+    >>> get_number_from_letter('Z')
+    25
+    >>> get_number_from_letter('X')
+    23
+    '''
+    
+
+
+def get_letter_from_number(n):
+    ''' (int) -> str
+
+    >>> get_letter_from_number(0)
+    'A'
+    >>> get_letter_from_number(2)
+    'C'
+    >>> get_letter_from_number(24)
+    'Y'
+    >>> get_letter_from_number(25)
+    'Z'
+    '''
+    
+
+
+
+def shift_26(n, shift):
+    ''' (int) -> int
+
+    Return n shifted by 
+
+    '''
+
+
+
+
+
